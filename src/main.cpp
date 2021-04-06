6a9affa2b0aba7d55a7772dfeffa76ab26848091
Karl-Heinrich Tamm <karl.heinrich@zoho.com>
2021-04-06 22:22:01 +0300

    Refining code

--- src/main.cpp
+++ src/main.cpp
@@ -9,20 +9,20 @@ const uint8_t DISPLAY_PIN = 3;
 const uint8_t CLK_PIN = 5;
 
 /* Colors */
-RGB YELLOW(245, 245, 66);
-RGB RED(245, 84, 66);
-RGB BLUE(66, 81, 245);
-RGB GOLD(245, 179, 66);
-RGB CRIMSON(220, 20, 60);
-RGB INDIGO(63, 0, 255);
+const RGB YELLOW(245, 245, 66);
+const RGB RED(245, 84, 66);
+const RGB BLUE(66, 81, 245);
+const RGB GOLD(245, 179, 66);
+const RGB CRIMSON(220, 20, 60);
+const RGB INDIGO(63, 0, 255);
 
 /* Gradients */
-Gradient YELLOW_GRAD(GOLD, YELLOW);
-Gradient RED_GRAD(CRIMSON, RED);
-Gradient BLUE_GRAD(INDIGO, BLUE);
+const Gradient YELLOW_GRAD(GOLD, YELLOW);
+const Gradient RED_GRAD(CRIMSON, RED);
+const Gradient BLUE_GRAD(INDIGO, BLUE);
 
 /* Sound */
-const uint16_t MIN_VOL = 270; // Measured volume value can be from 0 to 1023. With small amp. gain, everything below 270 is noise.
+const uint16_t MIN_VOL = 270; // Measured volume value can be from 0 to 1023. Consider everything below MIN_VOL as noise.
 const uint8_t LOCAL_MEASURING_INTERVAL = 100; // ms
 const uint16_t PEAK_SOUND_RESET_INTERVAL = 5000; // ms
 
@@ -55,23 +55,19 @@ void setup() {
     display.setBrightness(7);
 
     /* ColorWall test */
-    visible.print();
-    for(uint8_t i = 0; i < 6; i++){
-        visible.flowToRight();
-        buffer.flowInto(&visible);
-        buffer.flowToRight();
-    }
-    visible.print();
-    Serial.print("test");
+    // visible.print();
+    // for(uint8_t i = 0; i < 6; i++){
+    //     visible.flowToRight();
+    //     buffer.flowInto(&visible);
+    //     buffer.flowToRight();
+    // }
+    // visible.print();
 }
 
 void loop() {
-    /* Measure sound level */
-    uint16_t local_max_sound = getSoundVolume();
-
-    /* Update LED display */
+    /* Show sound volume */
     display.showNumberDec(8888);
-    uint8_t LED_brightness = map(local_max_sound, MIN_VOL, peak_sound, 1, 7);
+    uint8_t LED_brightness = map(getSoundVolume(), MIN_VOL, peak_sound, 1, 7);
     display.setBrightness(LED_brightness);
 }
 
